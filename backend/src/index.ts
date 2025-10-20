804eb22f0ad5bca3458a39fc28f6e8962d1169d9
aleksandar suletic <aleksandar.suletic@crayon.com>
2025-10-20 20:44:25 +0200

    added routes and model for hotel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,15 @@ import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
+import myHotelRoutes from './routes/my-hotels';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -34,6 +43,7 @@ app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use('/api/users', userRoutes); // if request URL starts with /api/users, forward it to userRoutes
 app.use('/api/auth', authRoutes);
+app.use('/api/my-hotels', myHotelRoutes);
 
 const PORT = process.env.PORT || 7000;
 
